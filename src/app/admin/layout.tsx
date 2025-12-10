2890cfe1f1f6b09f10d840169b8ccae7bcbfb04b
Yuber Laundry <yuberlaundry@gmail.com>
2025-12-10 14:43:58 +0000

    The page is still allowed to move the dashboard completely to the left.

--- src/app/admin/layout.tsx
+++ src/app/admin/layout.tsx
@@ -176,7 +176,7 @@ export default function AdminPortalLayout({
           <div className="flex-1" />
           <UserNav />
         </header>
-        <main className="flex-1 p-4 sm:p-6 lg:p-8">
+        <main className="p-4 sm:p-6 lg:p-8">
             {children}
         </main>
       </SidebarInset>
