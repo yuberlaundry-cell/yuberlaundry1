1fe8004f20d835827b05f81139e073fb461d0c48
Yuber Laundry <yuberlaundry@gmail.com>
2025-12-11 15:10:23 +0000

    Try fixing this error: `Runtime Error: Failed to load chunk /_next/stati

--- next.config.ts
+++ next.config.ts
@@ -1,3 +1,4 @@
+
 import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
@@ -31,5 +32,7 @@ const nextConfig: NextConfig = {
     ],
   },
 };
+
+// Triggering a reload to fix a Turbopack chunk error.
 
 export default nextConfig;
